76f6fcea7ab0bac509e175c46f54ff9bdd2ceecd
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-02 13:32:58 +0200

    Fix bugs with duplicate functions

--- handlers.js
+++ handlers.js
@@ -30,18 +30,6 @@ exports.welcome = function(request, response) {
     });
 }
 
-exports.upload = function(request, response) {
-    console.log("Rozpoczynam obsługę żądania upload.");
-    response.write("Rozpoczynam upload!");
-    response.end();
-}
-
-exports.welcome = function(request, response) {
-    console.log("Rozpoczynam obsługę żądania welcome.");
-    response.write("Witaj na stronie startowej!");
-    response.end();
-}
-
 exports.error = function(request, response) {
     console.log("Nie wiem co robić.");
     response.write("404 :(");

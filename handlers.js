e5fa733dc13bb9b67ad9bfe5d4ea9504d955435e
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-02 14:09:39 +0200

    Change rename function - uploading file has original name after upload

--- handlers.js
+++ handlers.js
@@ -1,12 +1,14 @@
 var fs = require('fs');
 var formidable = require('formidable');
+var image;
 
 exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload.");
     var form = new formidable.IncomingForm();
     form.parse(request, function(error, fields, files) {
-        fs.renameSync(files.upload.path, "test.png");
-        response.writeHead(200, { "Content-Type": "text/html" });
+        fs.renameSync(files.upload.path, files.upload.name);
+        image = files.upload.name;
+        response.writeHead(200, {"Content-Type": "text/html"});
         response.write("received image:<br/>");
         response.write("<img src='/show' />");
         response.end();
@@ -14,7 +16,7 @@ exports.upload = function(request, response) {
 }
 
 exports.show = function(request, response) {
-    fs.readFile("test.png", "binary", function(error, file) {
+    fs.readFile(image, "binary", function(error, file) {
         response.writeHead(200, {"Content-Type": "image/png"});
         response.write(file, "binary");
         response.end();
